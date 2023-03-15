56950b42944a1fa91053f82efce59338bfd9a264
Bastien Pruvost <bastien.mory@gmail.com>
2023-03-15 10:49:04 +0100

    chore: update config files update config files for eslint, prettier, typescript, next and editor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const withBundleAnalyzer = require('@next/bundle-analyzer')
+
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: { appDir: true },
-  images: { domains: ['avatars.githubusercontent.com'] },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    // optimizeCss: true,
+    // typedRoutes: true,
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;
